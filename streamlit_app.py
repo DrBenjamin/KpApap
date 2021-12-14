e9ab2ae2442eb4ee1c012259fb25d0cff0e73a80
Benjamin Groß <ben@benbox.org>
2021-12-14 15:27:10 +0100

    completed code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
+## Title and some information
 st.title('GEOMAGNETIC ACTIVITY')
 st.subheader('Planetary indicators of geomantic activity')
 st.write('The geomagnetic 3-hour Kp index was introduced in 1949 by J. Bartels and is calculated from the standardized K indices (Ks) of 13 geomagnetic observatories. It was developed to measure solar particle radiation via its magnetic effects and is now considered a proxy for the energy input from the solar wind into the Earth system.')
@@ -16,22 +16,29 @@ DATE_COLUMN = 'date/time'
 # Text Testfile: https://www.benbox.org/R/py/test.txt
 DATA_URL = ('https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_since_1932.txt')
 
+## Load data function
 @st.cache
 def load_data():
     colnames = ['Year', 'Month', 'Day', 'Hour', 'Minute', 'Days', 'Days_m', 'Kp', 'ap', 'D']
     data = pd.read_table(DATA_URL, sep = " ", header = None, names = colnames, skiprows = 31, skipinitialspace = True)
     return data
 
+# Show loading message
 data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text("Kp und ap downloaded!")
 
+## Checkbox for option to see raw data
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+## Plotting
 st.subheader('Diagram of geomagnetic activity')
+# create data for plotting
 data_plot = pd.DataFrame({"Year":data.Year,
                           "ap":data.ap})
+# set index (x-axis) to 'Year' column
+data_plot = data_plot.set_index('Year')
+# do the plotting
 st.line_chart(data_plot)
-st.altair_chart(data_plot)
