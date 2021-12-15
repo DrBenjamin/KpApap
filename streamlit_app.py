1974fee5bb9b120c541faa5aede768b48d89d3cb
Benjamin Groß <ben@benbox.org>
2021-12-15 12:10:43 +0100

    added average ap function and R code implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ DATE_COLUMN = 'date/time'
 # Link to data from Helmholtz-Zentrum Potsdam
 # https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_nowcast.txt
 # https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_since_1932.txt
-DATA_URL = ('https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_nowcast.txt')
+DATA_URL = ('https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_since_1932.txt')
 
 ## Load data function
 @st.cache
@@ -26,18 +26,49 @@ def load_data():
 # Show loading message
 data_load_state = st.text('Loading data...')
 data = load_data()
-data_load_state.text("Kp und ap downloaded!")
+data_load_state.text("ap data downloaded!")
 
 ## Checkbox for option to see raw data
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+## create a data frame
+data_cal = pd.DataFrame({"Year":pd.to_datetime(data.Year.map(str) + "-" + data.Month.map(str) + "-" + data.Day.map(str)),
+                          "ap":data.ap})
+                          
+## R code implementation
+# Define command and arguments
+command = 'Rscript'
+path2script = './max.R'
+
+# avg ap per day as a string list for args
+test_str = ""
+avg_ap_d = []
+avg_ap_d_t = []
+x = 0
+for i in range(1, len(data_cal)): 
+  if data_cal['Year'][i] == test_str or i == 1:
+    x = x + data_cal['ap'][i]
+    test_str = data_cal['Year'][i]
+  else :
+    x = x / 8
+    avg_ap_d_t.append(str(data_cal['Year'][i]))
+    avg_ap_d.append(int(np.ceil(x)))
+    x = 0
+    x = data_cal['ap'][i]
+    test_str = data_cal['Year'][i]
+args = list(map(str, avg_ap_d))
+# Build subprocess command
+cmd = [command, path2script] + args
+# check_output will run the command and print the result
+st.write('The maximum of the daily average ap was:', subprocess.check_output(cmd, universal_newlines = True))
+
 ## Plotting
 st.subheader('Diagram of geomagnetic activity')
-# create data for plotting
-data_plot = pd.DataFrame({"Year":pd.to_datetime(data.Year.map(str) + "-" + data.Month.map(str) + "-" + data.Day.map(str)),
-                          "ap":data.ap})
+# create dataframe with avg ap per day values
+data_plot = pd.DataFrame({'Year': pd.to_datetime(avg_ap_d_t),
+                         'ap': avg_ap_d})
 # set index (x-axis) to 'Year' column
 data_plot = data_plot.set_index('Year')
 # do the plotting
