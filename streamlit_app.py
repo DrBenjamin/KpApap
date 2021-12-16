2097a1c4c0eaa1b457056d5ea1d39ab7d979dfbe
Benjamin Groß <ben@benbox.org>
2021-12-16 04:58:00 +0100

    added MySQL support

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,30 @@ import pandas as pd
 import numpy as np
 import math
 import datetime
+import mysql.connector
+
+## SQL Connection
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from pets;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 ## Title and some information
 st.title('GEOMAGNETIC ACTIVITY')
