1234ae0113a5866cb98037bc3debfb1cb663e722
Benjamin Groß <ben@benbox.org>
2021-12-28 14:25:20 +0100

    fully translatable

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,21 @@ def run_query(query):
     return cur.fetchall()
 
 ## Title and some information
-st.title('GEOMAGNETIC ACTIVITY')
-st.subheader('Planetary indicators of geomantic activity')
-st.write('The geomagnetic 3-hour Kp index was introduced in 1949 by J. Bartels and is calculated from the standardized K indices (Ks) of 13 geomagnetic observatories. It was developed to measure solar particle radiation via its magnetic effects and is now considered a proxy for the energy input from the solar wind into the Earth system.')
-st.write('Because of the non-linear relationship of the K-scale to magnetometer fluctuations, it is not meaningful to take the average of a set of K-indices. Instead, every 3-hour K-value will be converted back into a linear scale called the a-index or just ap.')
+# Ask for language (whole site)
+lang = st.selectbox('In which language should this site appear?', ('BG', 'CS', 'DA', 'DE', 'EL', 'EN-GB', 'ES', 'ET', 'FI', 'FR', 'HU', 'IT', 'JA', 'LT', 'LV', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'ZH'), index = 5, key = 'lang')
+title = 'GEOMAGNETIC ACTIVITY'
+subheader = 'Planetary indicators of geomantic activity'
+info1 = 'The geomagnetic 3-hour Kp index was introduced in 1949 by J. Bartels and is calculated from the standardized K indices (Ks) of 13 geomagnetic observatories. It was developed to measure solar particle radiation via its magnetic effects and is now considered a proxy for the energy input from the solar wind into the Earth system.'
+info2 = 'Because of the non-linear relationship of the K-scale to magnetometer fluctuations, it is not meaningful to take the average of a set of K-indices. Instead, every 3-hour K-value will be converted back into a linear scale called the a-index or just ap.'
+if lang != 'EN-GB' :
+  title = str(trans(title, lang))
+  info1 = str(trans(info1, lang))
+  info2 = str(trans(info2, lang))
+  subheader = str(trans(subheader, lang))
+st.title(title)
+st.subheader(subheader)
+st.write(info1)
+st.write(info2)
 
 # Link to data from Helmholtz-Zentrum Potsdam
 # https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_nowcast.txt
@@ -50,13 +61,24 @@ def load_data():
   return data
 
 # Show loading message
-data_load_state = st.text('Loading data...')
+data_load_text = 'Loading data...'
+data_down_text = 'Data downloaded'
+if lang != 'EN-GB' :
+  data_load_text = trans(data_load_text, lang)
+  data_down_text = trans(data_down_text, lang)
+data_load_state = st.text(data_load_text)
 data = load_data()
-data_load_state.text('Data downloaded (' + str(round(sys.getsizeof(data)/1048576, 2)) + 'MB)!')
+data_load_state.text(str(data_down_text) + ' (' + str(round(sys.getsizeof(data)/1048576, 2)) + 'MB)!')
 
 ## Checkbox for option to see raw data
-if st.checkbox('Show raw data'):
-  st.subheader('Raw data')
+check1 = 'Show raw data?'
+if lang != 'EN-GB' :
+  check1 = trans(check1, lang)
+if st.checkbox(str(check1)):
+  check1sub = 'Raw data'
+  if lang != 'EN-GB' :
+    check1sub = trans(check1sub, lang)
+  st.subheader(check1sub)
   st.write(data)
 # Create a data frames
 data_plot_td = pd.DataFrame({'Time': data.Hour, 'ap': data.ap}).tail(8)
@@ -80,7 +102,7 @@ def calc_top10(date, ap):
       avg_ap_d_t.append(str(test_str))
       avg_ap_d.append(int(np.ceil(x)))
       for y in range(0, 10, 1) :
-        # creating top 10 list
+        # Creating top 10 list
         if x > max_ap[y][0] :
           max_ap[y][0] = int(np.ceil(x))
           max_ap[y][1] = str(test_str)[0:10]
@@ -91,7 +113,7 @@ def calc_top10(date, ap):
   return avg_ap_d, avg_ap_d_t, max_ap
 # Call function to do the calculation
 avg_ap_d, avg_ap_d_t, max_ap = calc_top10(date = data_cal['Date'], ap = data_cal['ap'])
-# create dataframe with avg ap per day values
+# Create dataframe with avg ap per day values
 data_plot = pd.DataFrame({'Date': pd.to_datetime(avg_ap_d_t),
                          'ap': avg_ap_d})
                          
@@ -100,10 +122,15 @@ data_plot = pd.DataFrame({'Date': pd.to_datetime(avg_ap_d_t),
 data_plot_td = data_plot_td.set_index('Time')
 data_plot = data_plot.set_index('Date')
 # Daily activity
-st. subheader('Diagram of today`s geomagnetic activity')
+plot1_subheader = 'Diagram of today`s geomagnetic activity'
+plot2_subheader = 'Diagram of geomagnetic activity since 1932'
+if lang != 'EN-GB' :
+  plot1_subheader = trans(plot1_subheader, lang)
+  plot2_subheader = trans(plot2_subheader, lang)
+st. subheader(plot1_subheader)
 st.bar_chart(data_plot_td)
 # All data plot
-st.subheader('Diagram of geomagnetic activity since 1932')
+st.subheader(plot2_subheader)
 st.line_chart(data_plot)
 
 ## Show 10 maximum ap days
@@ -113,21 +140,50 @@ max_ap_data['Date'] = [sublist[1] for sublist in max_ap]
 max_ap_data['ap'] = [sublist[0] for sublist in max_ap]
 max_ap_data_index = pd.Index(range(1, 11, 1))
 max_ap_data = max_ap_data.set_index(max_ap_data_index)
-# Show top 10 data                    
-st.write('The maximum of the daily average ap was on ', str(max_ap[0][1]), ' at ', str(max_ap[0][0]), '.')
-if st.checkbox('Show max. ap days'):
-  st.subheader('Top 10 max. ap')
+# Show top 10 data
+info_maxap = 'The maximum of the daily average ap was on '
+if lang != 'EN-GB' :
+  info_maxap = trans(info_maxap, lang)
+st.write(str(info_maxap), str(max_ap[0][1]), ' at ', str(max_ap[0][0]), '.')
+check_maxap = 'Show ten maximum days?'
+check_maxapsubheader = 'Top 10'
+if lang != 'EN-GB' :
+  check_maxap = trans(check_maxap, lang)
+  check_maxapsubheader = trans(check_maxapsubheader, lang)
+if st.checkbox(str(check_maxap)):
+  st.subheader(check_maxapsubheader)
   st.write(max_ap_data)
   
 ## Use local databank
-if st.checkbox('Use local databank?'):
+use_databank = 'Use local databank?'
+if lang != 'EN-GB' :
+  use_databank = trans(use_databank, lang)
+if st.checkbox(str(use_databank)):
   conn = init_connection()
   # Ask for events
-  date = st.selectbox('On which day was the event?', max_ap_data, key = 'date')
-  st.write('You selected:', date)
-  event = st.text_input('What happened on this day?', placeholder = 'All kinds of events', key = 'event')
+  day_event = 'On which day was the event?'
+  text_input = 'What happened on this day?'
+  text_output = 'You selected'
+  text_placeholder = 'All kinds of events'
+  if lang != 'EN-GB' :
+    day_event = trans(day_event, lang)
+    text_input = trans(text_input, lang)
+    text_output = trans(text_output, lang)
+    text_placeholder = trans(text_placeholder, lang)
+  date = st.selectbox(str(day_event), max_ap_data, key = 'date')
+  st.write(str(text_output), date)
+  event = st.text_input(str(text_input), placeholder = text_placeholder, key = 'event')
   # Write data to databank
-  if st.button('Store in databank?'):
+  stored_data = 'Store in databank?'
+  stored_datasuccess = 'stored to databank!'
+  show_data = 'Show databank data?'
+  show_datasubheader = 'Databank data'
+  if lang != 'EN-GB' :
+    stored_data = trans(stored_data, lang)
+    stored_datasuccess = trans(stored_datasuccess, lang)
+    show_data = trans(show_data, lang)
+    show_datasubheader = trans(show_datasubheader, lang)
+  if st.button(str(stored_data)):
   # Check for ID number
     id = 0
     query = "SELECT ID from `dbs5069306`.`topten`;"
@@ -141,20 +197,15 @@ if st.checkbox('Use local databank?'):
     query = "INSERT INTO `dbs5069306`.`topten` VALUES ('%s', '%s', '%s');" %(id, date, event)
     run_query(query)
     conn.commit()
-    st.write(date, event, ' stored to databank!')
+    st.write(date, event, ' ', stored_datasuccess)
   # Checkbox for option to see databank data
-  if st.checkbox('Show databank data'):
-    st.subheader('Databank data')
+  if st.checkbox(str(show_data)):
+    st.subheader(show_datasubheader)
     query = "SELECT * from `dbs5069306`.`topten`;"
     rows = run_query(query)
-    lang = st.selectbox('In which language should the event text be translated?', ('BG', 'CS', 'DA', 'DE', 'EL', 'EN-GB', 'ES', 'ET', 'FI', 'FR', 'HU', 'IT', 'JA', 'LT', 'LV', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'ZH'), index = 3, key = 'lang')
     databank = pd.DataFrame(columns = ['ID', 'Date', 'Event'])
     for row in rows:
-      if lang != 'EN-GB' :
-        trans_event = str((trans(row[2], lang)))
-      else:
-        trans_event = row[2]
-      df = pd.DataFrame([[row[0], row[1], trans_event]], columns = ['ID', 'Date', 'Event'])
+      df = pd.DataFrame([[row[0], row[1], row[2]]], columns = ['ID', 'Date', 'Event'])
       databank = databank.append(df)
     # Print databank in dataframe table
     databank = databank.set_index('ID')
